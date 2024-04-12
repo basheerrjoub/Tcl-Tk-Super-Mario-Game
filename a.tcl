d6be3c410e4122afb531eb637491bf9058611dad
basheerrjoub <basheer20599@gmail.cm>
2024-04-12 17:29:45 +0200

    Adding Goomba

--- a.tcl
+++ a.tcl
@@ -18,6 +18,13 @@ set m_y 600
 set marioCharImg [image create photo -file "images/mario_char.png"]
 set marObj [.myCanvas create image $m_x $m_y -image $marioCharImg]
 
+set g_x 400
+set g_y 600
+# Goomba Character Image
+set goombaCharImg [image create photo -file "images/goomba.png"]
+set goombaObj [.myCanvas create image $g_x $g_y -image $goombaCharImg]
+
+
 # Character Movement Procedure
 proc move { obj x y } {
     global m_x m_y keysym
@@ -29,6 +36,42 @@ proc move { obj x y } {
     }
     set keysym "$m_x and $m_y"
     return
+}
+
+
+
+# Character Movement Procedure
+proc move_g { obj x y } {
+    global g_x g_y
+    if {([expr {$g_x + $x}] < 1000) && ([expr {$g_x + $x}] > 30)} {
+      .myCanvas move $obj $x $y
+      set g_x [expr {$g_x + $x}]
+      set g_y [expr {$g_y + $y}]
+
+    }
+    return
+}
+
+
+
+# Enemy Follow X
+proc follow {obj} {
+  global m_x m_y g_x g_y
+  set magnitude [expr {$g_x - $m_x}]
+  set dx 0
+  if {$magnitude > 0} {
+    set dx -10
+  } elseif {$magnitude < 0} {
+    set dx 10
+  } else {
+    # Lost Game 
+  
+  set reply [tk_dialog .myCanvas.foo "You Lost" "Do you want to Play Again?" \
+        questhead 0 Yes No "I'm not sure"]
+  }
+  
+  move_g $obj $dx 0
+
 }
 
 pack [label .l -textvariable keysym -padx 2m -pady 1m]
@@ -54,5 +97,8 @@ bind . <Key> {
   } elseif {[string equal %K "Left"]} {
     set dx -10
   }
-  move $marObj $dx $dy 
+  move $marObj $dx $dy
+  follow $goombaObj
 }
+
+
